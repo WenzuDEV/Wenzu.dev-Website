056216eda2c91c079473fb2c03648b6def6dd398
Wenzu.dev <wenzuyt@gmail.com>
2025-11-09 18:27:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 30) {
+    navbar.style.background = 'rgba(0,0,0,0.85)';
+  } else {
+    navbar.style.background = 'rgba(0,0,0,0.6)';
+  }
+});
